682165fa22418450d081f0b60011476d45aa5b57
jackyyp <chpoonal@connect.ust.hk>
2023-03-13 00:36:54 +0800

    test

--- notes/mil-bug.cpp
+++ notes/mil-bug.cpp
@@ -13,7 +13,7 @@ class Word{
         {str = new char [strlen(s)+1];strcpy(str,s);}
         ~Word(){delete[] str;}
         void print() const {cout << frequency << " " << str << endl;}
-
+        char* get_str(){return this->str;}
         void solve_bug(){str=nullptr;}
 };
 
@@ -23,4 +23,4 @@ void Bug(Word& x){Word bug("bug",4); x = bug;}
 // in Bug(), we are creating a new object, x.str now points to what bug.str points to (shallow copy)
 //when bug() is out of scope, it will delete bug obj, including what x.str points to.
 // when main() is out of scope, it will delete x obj, also what x.str points to, causing double delete.
-int main(){Word movie {"Titanic"};Bug(movie);movie.solve_bug();return 0;}
+int main(){Word movie {"Titanic"};Bug(movie);return 0;}
