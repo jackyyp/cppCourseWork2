bf2f3c8e59362e8557b6e12877ab92cbb190fa28
jackyyp <chpoonal@connect.ust.hk>
2023-03-15 02:27:51 +0800

    leak check tmr

--- PA1/player.cpp
+++ PA1/player.cpp
@@ -21,19 +21,17 @@ Player::~Player(){
 //task 2 : Implement class PlayerList's default constructor, copy constructor and destructor
 
 //sets the number of players to 0 and assign nullptr to the players array. 
-PlayerList::PlayerList(){
-    this->numPlayers = 0;
-    this->players = nullptr; //2D player** ptr
-}
+PlayerList::PlayerList(): numPlayers(0), players(nullptr){};
+
 
 //The copy constructor allocates memory for the players array and copy data from the passed list to the new array.
 PlayerList::PlayerList(const PlayerList& list){
     //deepcopy of player* , not player
-    this->numPlayers = list.numPlayers; // size of array
+    this->numPlayers = list.getNumPlayers(); // size of array
 
     this->players = new Player*[this->numPlayers];
     for(int i=0;i<this->numPlayers;i++){
-        this->players[i]  = list.players[i]; //assign by element.
+        this->players[i]  = list.getPlayer(i); //assign by element.
     }
 }
 
@@ -45,7 +43,7 @@ PlayerList::~PlayerList(){
 //Task 3: Implement the following 3 functions
 
 
-//This function adds a Player pointer to the list. 
+//This function adds a Player pointer to end of the list. 
 //You will need to handle the dynamically allocated array correctly, increase its size by allocating a new array if necessary.
 void PlayerList::addPlayer(Player* const player){
     //create new dynamic array with size+1. copy ptr in array.
@@ -53,7 +51,8 @@ void PlayerList::addPlayer(Player* const player){
     for(int i=0;i<this->numPlayers;i++){
         temp[i] = this->players[i];
     }
-    temp[this->numPlayers] = player; // add the new player* at last
+    
+    temp[this->numPlayers] = player; // add the new player* at end
 
     delete[] this->players; // delete original ptr
     this->players = temp; // assign ptr to new array
@@ -67,15 +66,6 @@ void PlayerList::addPlayer(Player* const player){
 
 
 //*helper function*
-void swap(const Player* ptr1, const Player* ptr2){
-    //ptr to constant player, as we should not modify player obj.
-    const Player* temp = ptr1; 
-    ptr1 = ptr2;
-    ptr2 = temp;
-
-    temp = nullptr; //sanity check
-    return;
-}
 
 //check condition ( if sorted by desc).
 bool isAllEqualScore(const PlayerList& list){
@@ -93,28 +83,41 @@ bool isAllEqualScore(const PlayerList& list){
 
 //We implement Insertion sort here.
 void PlayerList::sort(){
-    if(!isAllEqualScore(*this)){ // sort by elo first
-        for(int i =1 ; i<this->getNumPlayers();i++){ // if i=0 , it wont run the for loop.
+    
+        for(int i =1 ; i<this->getNumPlayers();i++){ // if i=1 , it wont run the for loop.
             int j = i-1;
 
             while(j>=0 && this->getPlayer(j)->getELO() < this->getPlayer(j+1)->getELO()){ //sort desc.
-                swap(this->getPlayer(j),this->getPlayer(j+1));   //  1 2 3 -> 2 1 3 -> 2 3 1 -> 3 2 1
+                //swap the *value* of the 2 corresponding pointers in the array.
+                Player* temp = this->players[j];
+                this->players[j] = this->players[j+1];
+                this->players[j+1] = temp;
+
+                temp = nullptr;
+
+                //  1 2 3 -> 2 1 3 -> 2 3 1 -> 3 2 1
                 j--;
             }
 
         }
-    }
-    else{ //sort by score , as We call sort() again.
+    
+        //sort by score , as We call sort() again.
         for(int i =1 ; i<this->getNumPlayers();i++){ // if i=0 , it wont run the for loop.
             int j = i-1;
 
             while(j>=0 && this->getPlayer(j)->getScore() < this->getPlayer(j+1)->getScore()){ //sort desc.
-                swap(this->getPlayer(j),this->getPlayer(j+1));   //  1* 1 3 -> 1* 3 1 - > 3 1* 1 , elo order preserved.
+                Player* temp = this->players[j];
+                this->players[j] = this->players[j+1];
+                this->players[j+1] = temp;
+
+                temp = nullptr;
+
+                   //  1* 1 3 -> 1* 3 1 - > 3 1* 1 , elo order preserved.
                 j--;
             }
 
         }
-    }
+    
 
     return;
 }
--- PA1/swiss.cpp
+++ PA1/swiss.cpp
@@ -1,2 +1,93 @@
+#include "swiss.h"
+#include "match.h"
 
 
+
+//Note that after initializing list, you should perform "seeding" for the tournament by calling list.sort().
+//To validate, after creating a Swiss instance, calling printLeaderboard() should print all players in decreasing order of ELO rating.
+
+//conversion constructor
+
+Swiss::Swiss(const int numRounds, const PlayerList& list): numRounds(numRounds), curRound(0), list(list){
+    //sort by elo.
+    this->list.sort();
+}
+//Hint: list is a static-allocated data member of Swiss, so you should not use new or delete for this data member.
+//playerlist obj handle its own destruction.
+Swiss::~Swiss(){
+}
+
+//This function will update curRound accordingly, 
+//and you will need to make use of the provided printLeaderboard() function in swiss.h to print the list of players and their scores after each round.
+void Swiss::play(){
+    //round 0 is handled by main.cpp
+    this->curRound++;
+
+    
+    for(int i=1;i<=this->numRounds;i++){
+        
+        //Create an array of PlayerList, where each list consists of all players with the same score.
+        int numscore = 2*this->curRound-1;
+        PlayerList** split = new PlayerList* [numscore]; // delete every playlist* later, Playerlist have its own destructor.
+
+        //**dont forget to allocate memory...
+        for(int i=0;i<numscore;i++){
+            split[i] = new PlayerList;
+        }
+
+        //load the every player* into split.       
+
+
+        for(int j=0;j<this->list.getNumPlayers();j++){
+            int score =  this->list.getPlayer(j)->getScore();
+            //put into diff. playerlist according to score. 
+
+            split[score]->addPlayer(this->list.getPlayer(j));
+
+        
+        }
+
+
+
+        //For each non-empty PlayerList, starting from the highest score
+        //sort every list
+        for(int j=numscore-1;j>=0;j--){
+            //is empty
+            if(split[j]->getNumPlayers()==0){continue;}
+            
+            split[j]->sort(); //as score changed.
+           
+        
+
+            int numPlayerInSplit =  split[j]->getNumPlayers();
+  
+
+            for(int k=0;k<numPlayerInSplit/2;k++){
+                //construct match
+                Match match {split[j]->getPlayer(k),split[j]->getPlayer((numPlayerInSplit/2)+k)};
+                match.play();
+            }
+
+            //pushdown the remaining player if exist(odd):
+            if(numPlayerInSplit%2!=0){
+                //if last row still hv player ,he would gain 2 points without playing in any match.
+                if(j-1<0){
+                    split[j]->getPlayer(numPlayerInSplit-1)->addScore(2);
+                }
+                else{
+                    //append at top of next split
+                   split[j-1]->addPlayer(split[j]->getPlayer(numPlayerInSplit-1));
+                }
+            }
+
+        }
+
+        //Sort the list after all matches have been played, then call printLeaderboard().
+        this->list.sort();
+        this->printLeaderboard();
+        this->curRound++;
+
+        //Delete any memory dynamically allocated in the loop.
+        delete[] split;
+    }
+}
